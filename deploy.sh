0003e08428f208f4834b3da2ecf35df7fa7749c8
John T Skarbek <jtslear@gmail.com>
2017-04-17 14:27:31 -0400

    test

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,17 @@ declare -r changed=$(git diff --name-only HEAD~1)
 
 echo ${changed}
 
-if [[ "app-config.yml" == ${changed} ]]
+containsElement () {
+  local e
+  for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
+  return 1
+}
+
+if [[ $(containsElement "app-config.yml" "${changed}") ]]
 then
   echo "Gonna reconfigure the application"
+else
+  echo "No application configuration changes detected"
 fi
 
 
