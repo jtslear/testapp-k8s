5553a0054b4b04a541000733bb0d212eca8a09c4
John T Skarbek <jtslear@gmail.com>
2017-04-17 14:50:01 -0400

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 declare -r changed=$(git diff --name-only HEAD~1)
 
@@ -8,7 +8,10 @@ containsElement () {
   local e
   for e in "${@:2}"
   do
-    [[ "$e" == "$1" ]] && return 0
+    if [[ ${e} == ${1} ]]
+    then
+      return 0
+    fi
   done
   return 1
 }
@@ -17,7 +20,7 @@ containsElement "app-config.yml" "${changed[@]}"
 declare -r is_changed=$?
 echo ${is_changed}
 
-if [[ ${is_changed} -eq 1 ]]
+if [[ ${is_changed} -eq 0 ]]
 then
   echo "Gonna reconfigure the application"
 else
