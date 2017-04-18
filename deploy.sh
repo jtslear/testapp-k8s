6fc1ba32f176e48f4382e378458a763346467b18
John T Skarbek <jtslear@gmail.com>
2017-04-18 08:58:45 -0400

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,30 @@
 #!/bin/bash -x
 
-declare -a changed=($(git diff --name-only HEAD~1))
+declare -r image=jtslear/testapp-k8s
+declare -r deployment=testapp
 
-echo ${changed}
+declare -a files_changed=($(git diff --name-only HEAD~1))
 
-containsElement () {
+echo ${files_changed}
+
+elementIn () {
   local e
   for e in "${@:2}"; do [[ "$e" == "$1" ]] && return 0; done
   return 1
 }
 
-containsElement "app-config.yml" "${changed[@]}"
-declare -r is_changed=$?
-echo ${is_changed}
+elementIn "app-config.yml" "${files_changed[@]}"
+declare -r config_is_changed=$?
+echo ${config_is_changed}
 
-if [[ ${is_changed} -eq 0 ]]
+if [[ ${config_is_changed} -eq 0 ]]
 then
   echo "Gonna reconfigure the application"
+  kubectl create -f ./kubernetes/
 else
   echo "No application configuration changes detected"
+  echo "Proceeding to complete a deploy..."
+  kubectl set image deployments/${deployment} ${image}=${image}:${CIRCLE_SHA1}
 fi
 
 
